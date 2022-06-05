4ab10a07f009369c7133539c98ea7d351d6798c3
Josie G.F Purba <josepurba2001@gmail.com>
2022-06-05 13:21:18 +0700

    up seeds

--- db/seeds/20220605060706-Type_Users.js
+++ db/seeds/20220605060706-Type_Users.js
@@ -1,24 +1,31 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(
+            "Type_Users",
+            [
+                {
+                    type_user: "Superadmin",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    type_user: "Admin",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    type_user: "Member",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete("Type_Users", null, {});
+    },
 };
